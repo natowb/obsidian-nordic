443bc06f14204feaea0de80d4f078183ec6f9fef
Nato <44450112+natowb@users.noreply.github.com>
2024-03-15 08:31:34 +1300

    first commit

---
+++ tools/build.mjs
@@ -0,0 +1,24 @@
+import { render } from 'node-sass';
+import { writeFile } from 'fs';
+
+// Input and output files
+const inputFile = './src/styles.scss';
+const outputFile = './theme.css';
+
+// Compile SCSS/SASS file
+render({
+    file: inputFile,
+    outFile: outputFile,
+}, (error, result) => {
+    if (error) {
+        console.error('Error compiling SCSS:', error);
+    } else {
+        writeFile(outputFile, result.css, err => {
+            if (err) {
+                console.error('Error writing CSS file:', err);
+            } else {
+                console.log(`Compiled ${inputFile} -> ${outputFile}`);
+            }
+        });
+    }
+});
